df86df1297e88b9f13af044d8587191795bf11cf
Takumi-metro <takumi.suzuki@metroengines.jp>
2024-02-26 19:31:29 +0900

    基本設定

---
+++ app/api/date.js
@@ -0,0 +1,21 @@
+// app/api/data.js
+import mysql from 'mysql2/promise';
+
+export async function get(req, res) {
+  const connection = await mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  });
+
+  try {
+    const [rows] = await connection.execute('SELECT * FROM revenue_prod.hotels');
+    res.status(200).json(rows);
+  } catch (error) {
+    console.error('MySQL Error: ', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  } finally {
+    await connection.end();
+  }
+}
