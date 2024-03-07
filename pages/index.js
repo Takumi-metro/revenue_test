6e865d5342f4766ff6f5a7c0da7d9d09c28c0340
Takumi-metro <takumi.suzuki@metroengines.jp>
2024-03-07 18:51:46 +0900

    グラフ表記

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,42 @@
 import React, { useEffect, useState } from 'react';
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/styles/ag-grid.css'; // ag-Gridの基本スタイル
-import 'ag-grid-community/styles/ag-theme-alpine.css'; // ag-Gridのダークテーマ
+import { Line } from 'react-chartjs-2';
+import 'chart.js/auto';
 
 export default function Home() {
-  const [bookingSummary, setBookingSummary] = useState([]);
+  const [bookingData, setBookingData] = useState({ labels: [], datasets: [] });
 
   useEffect(() => {
     fetch('http://localhost:8000/booking_summary/')
       .then(response => response.json())
-      .then(data => setBookingSummary(data))
+      .then(data => {
+        if (data && Array.isArray(data)) { // dataが配列であることを確認
+          //const slicedData = data.slice(0, 100);
+          //const bookingDates =  slicedData.map(item => item.booking_date);
+          //const guestRooms = slicedData.map(item => item.rooms_count);
+          const bookingDates = data.map(item => item.booking_date);
+          const guestRooms = data.map(item => item.rooms_count);
+          setBookingData({
+            labels: bookingDates,
+            datasets: [
+              {
+                label: 'Rooms count',
+                data: guestRooms,
+                fill: false,
+                borderColor: 'rgba(75,192,192,1)',
+                tension: 0.1
+              }
+            ]
+          });
+        } else {
+          console.error('Invalid data format');
+        }
+      })
       .catch(error => console.error('Error fetching booking summary:', error));
   }, []);
 
-  // ag-Gridの列定義
-  const columnDefs = [
-    { headerName: "Booking Date", field: "booking_date" },
-    { headerName: "Group Date", field: "group_date" },
-    { headerName: "Rooms Count", field: "rooms_count" },
-    { headerName: "New Added", field: "new_added" },
-    { headerName: "New Cancelled", field: "new_cancelled" },
-    { headerName: "Total Guest Rooms", field: "total_guest_rooms" }
-  ];
-
   return (
-    <div className="ag-theme-alpine-dark" style={{ height: 600, width: '100%' }}>
-      <AgGridReact
-        rowData={bookingSummary}
-        columnDefs={columnDefs}
-      />
+    <div>
+      <Line data={bookingData} />
     </div>
   );
 }
