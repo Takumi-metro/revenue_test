a4b4bc08189e0fb5a1f0eed5ed7fb6d0644606e1
Takumi-metro <takumi.suzuki@metroengines.jp>
2024-03-08 11:35:46 +0900

    テキストアウトプット

---
+++ pages/list.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css'; // ag-Gridの基本スタイル
+import 'ag-grid-community/styles/ag-theme-alpine.css'; // ag-Gridのダークテーマ
+
+export default function Home() {
+  const [bookingSummary, setBookingSummary] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/booking_summary/')
+      .then(response => response.json())
+      .then(data => setBookingSummary(data))
+      .catch(error => console.error('Error fetching booking summary:', error));
+  }, []);
+
+  // ag-Gridの列定義
+  const columnDefs = [
+    { headerName: "Booking Date", field: "booking_date" },
+    { headerName: "Group Date", field: "group_date" },
+    { headerName: "Rooms Count", field: "rooms_count" },
+    { headerName: "New Added", field: "new_added" },
+    { headerName: "New Cancelled", field: "new_cancelled" },
+    { headerName: "Total Guest Rooms", field: "total_guest_rooms" }
+  ];
+
+  return (
+    <div className="ag-theme-alpine-dark" style={{ height: 600, width: '100%' }}>
+      <AgGridReact
+        rowData={bookingSummary}
+        columnDefs={columnDefs}
+      />
+    </div>
+  );
+}
